ffad833f098bb7cef84770f514d4e274e5bb4ea9
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-13 00:33:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ class Song
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
-    # @@artists << artist
+    @@artists << artist
     @genre = genre
     # @@genre << genre
     @@count += 1
